cadf119d71460a9cea33bca83e777155ce1cac28
Jovyan 123 <jovyan123.playground@gmail.com>
2021-03-08 05:11:27 -0600

    Fix changelog formatting

--- release_helper/cli.py
+++ release_helper/cli.py
@@ -463,7 +463,7 @@ def prep_changelog(branch, remote, repo, auth, path, resolve_backports, keep):
     # Insert the entry into the file
     # Test if we are augmenting an existing changelog entry (for new PRs)
     # Preserve existing PR entries since we may have formatted them
-    new_entry = f"{START_MARKER}\n{entry}\n{END_MARKER}\n"
+    new_entry = f"{START_MARKER}\n\n{entry}\n\n{END_MARKER}\n"
     prev_entry = changelog[changelog.index(START_MARKER) : changelog.index(END_MARKER)]
 
     if f"# {version}" in prev_entry:
